0ea8ad200b8c34fbfda0fdc535497a06b437c9d9
Mary Balukleeva <mary.balkleeva10@gmail.com>
2020-09-08 19:51:30 +0300

    Updates

---
+++ precache-manifest.5ffb3f87005e7be6d011fedaf1d50e2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977ecf3a56263cd0ddb9ff6e6f072789",
+    "url": "/gil-test-task/index.html"
+  },
+  {
+    "revision": "c1168fed9b503ff1b3b5",
+    "url": "/gil-test-task/static/css/main.0388dc9f.chunk.css"
+  },
+  {
+    "revision": "b675fad9114c50b95f19",
+    "url": "/gil-test-task/static/js/2.a89ab60e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gil-test-task/static/js/2.a89ab60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1168fed9b503ff1b3b5",
+    "url": "/gil-test-task/static/js/main.bbc8c034.chunk.js"
+  },
+  {
+    "revision": "013e50ba7b895ed803d8",
+    "url": "/gil-test-task/static/js/runtime-main.bbcbfbe6.js"
+  },
+  {
+    "revision": "48bd8f7d9cf2267271abc6568b432597",
+    "url": "/gil-test-task/static/media/arrow_left.48bd8f7d.svg"
+  },
+  {
+    "revision": "845e73a46e444a53fdba4f17b315ca27",
+    "url": "/gil-test-task/static/media/arrow_right.845e73a4.svg"
+  },
+  {
+    "revision": "907458b4c98cef66fe2aa6534e660805",
+    "url": "/gil-test-task/static/media/card-baack.907458b4.png"
+  }
+]);

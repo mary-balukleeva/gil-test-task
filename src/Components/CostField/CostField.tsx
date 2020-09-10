ccef7cce87ca9f66f49ac9a9d16dac3f2b812622
Mary Balukleeva <mary.balkleeva10@gmail.com>
2020-09-10 15:14:56 +0300

    add custom caret on input field

--- src/Components/CostField/CostField.tsx
+++ src/Components/CostField/CostField.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 import dollarPng from '../../assets/images/dollar-input.png';
 import './CostField.css';
 
 export default function CostField() {
+  const ref: any = useRef();
+  const divRef: any = useRef();
+
   const [cost, setCost] = useState('');
+  const [visibleCaret, toggleCaret] = useState(false);
 
   const formatCurrency = (value: number): string => {
     const format = new Intl.NumberFormat('en-us');
@@ -15,20 +19,22 @@ export default function CostField() {
 
   const onCostChange = (value: string) => {
     const valueExistDot = value.match(/\./g)!;
-    const dotOnEnd = value[value.length-1] === '.';
-
-    if (valueExistDot && valueExistDot.length > 1) { return; }
-
+    
+    const anyDotInField = valueExistDot && valueExistDot.length > 1;
+    if (anyDotInField) { return; }
+    
+    const dotOnEnd = /\.$/.test(value);
     const decimalPart: any = value.match(/\.\d+/);    
     const isInvalidDecimalLength: boolean = decimalPart && decimalPart[0].length > 4;
+
     if (isInvalidDecimalLength) { return; }
 
-    if (value && valueExistDot && valueExistDot.length && dotOnEnd) {
+    const isEmptyOrEndOnDotValue = !value || (value && valueExistDot && valueExistDot.length && dotOnEnd);
+    if (isEmptyOrEndOnDotValue) {
       return setCost(value);
     }
 
     const number: number = +value.replace(/[^\d\.]/g, '');
-
     setCost(formatCurrency(number));
   }
 
@@ -36,15 +42,21 @@ export default function CostField() {
     <div className="costFieldWrapper">
       <div className="cost-form">
         <TextField
+          ref={ref}
           id="cost-input"
           label="Treatment Cost *"
           type="text"
           variant="outlined"
           value={cost}
           onChange={e => onCostChange(e.target.value)}
+          onFocus={() => toggleCaret(true)}
+          onBlur={() => toggleCaret(false)}
         />
+        { visibleCaret && <div ref={divRef} className='divAfterInput' >
+          <span className="content">{cost}</span>
+          <div className="caret" />
+        </div>}
         <img src={dollarPng} alt="dollar" />
-
       </div>
     </div>
   );
